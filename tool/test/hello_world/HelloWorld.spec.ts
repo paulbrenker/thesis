5e5e2c51ea2bf55b1f48a65ff96eebdb2258da62
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2024-08-25 13:31:58 +0200

    remove hello world package

--- tool/test/hello_world/HelloWorld.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HelloWorld } from '../../src/hello_world/HelloWorld'
-
-describe('HelloWorld', () => {
-  it('should return "Hello World!" when sayHello is called', () => {
-    expect(HelloWorld.sayHello()).toBe('Hello World!')
-  })
-})
