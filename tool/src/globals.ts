a2bf5a8c7e8b6ac029a74c55099a6f713c9b0e46
Paul Brenker <paul.brenker@leanix.net>
2024-08-28 00:22:34 +0200

    comment why unlintable

--- tool/src/globals.ts
+++ tool/src/globals.ts
@@ -5,6 +5,11 @@ class Globals {
 
   static linterResults = `${Globals.data}/linter-results`
 
+  /*
+   manually determined OpenAPI Specifications that jamm the linter.
+   Linter returns no Promise.resolve even after more than 20 min.
+   Length of Sets does not justify this
+  */
   static unlintableSpecs = [
     '../data/openapi-directory/APIs/beezup.com/2.0/openapi.yaml',
     '../data/openapi-directory/APIs/microsoft.com/graph-beta/1.0.1/openapi.yaml'
