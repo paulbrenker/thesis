c223a88f0afdbb0aa882d5ddc977c88c011e1b0d
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2024-09-17 12:04:34 +0200

    remove comments from script

--- script/export_diagrams.sh
+++ script/export_diagrams.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Ensure that drawio is installed, if not install via snap
 if ! command -v drawio &> /dev/null; then
     echo "drawio is not installed. Installing drawio via snap..."
     sudo snap install drawio
@@ -10,24 +9,18 @@ if ! command -v drawio &> /dev/null; then
     fi
 fi
 
-# Define source and destination directories
 SOURCE_DIR="requirements"
 DEST_DIR="tex/thesis/img"
 
 SCALE=2
 
-# Create the destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
 
-# Find all .drawio files in the source directory and its subdirectories
 find "$SOURCE_DIR" -type f -name "*.drawio" | while read -r DRAWIO_FILE; do
-    # Extract the filename without extension
     FILE_NAME=$(basename "$DRAWIO_FILE" .drawio)
     
-    # Define the output PNG file path
     OUTPUT_FILE="$DEST_DIR/$FILE_NAME.png"
     
-    # Export the .drawio file to PNG
     if drawio -x -f png -o "$OUTPUT_FILE" -s $SCALE "$DRAWIO_FILE"; then
         echo "Exported: $DRAWIO_FILE -> $OUTPUT_FILE"
     else
