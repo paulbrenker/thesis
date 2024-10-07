3954902184dbe24091505c7ea3f898ca84afcfe4
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2024-10-07 19:45:11 +0200

    add statistical test for azure excluded rules

--- analysis/src/table/violationpartitionazure.py
+++ analysis/src/table/violationpartitionazure.py
@@ -5,6 +5,7 @@
 
 import logging
 import json
+from scipy import stats
 from communication import get_linter_results, DataFrameMappers, ColumnLimiters
 
 logger = logging.getLogger(__name__)
@@ -40,9 +41,45 @@ def create_report():
         .rename(columns={0: "azure"})
         .assign(total=df_total)
         .assign(percentage=((df_azure / df_total) * 100).round(decimals=1))
-        .fillna({"percentage": 0})
+        .dropna()
     )
 
     logger.info(
         "Azure Thrown Violations vs total thrown violations overview: \n%s", df_both
+    )
+
+    df_azure_not_azure_ignored = get_linter_results(
+        column_limiter=ColumnLimiters.get_azure_excluded(),
+        cell_mapper=DataFrameMappers.map_to_thrown,
+        index_limiter="azure.com",
+    ).sum()
+
+    df_total_not_azure_ignored = get_linter_results(
+        column_limiter=ColumnLimiters.get_azure_excluded(),
+        cell_mapper=DataFrameMappers.map_to_thrown,
+    ).sum()
+
+    df_both_not_azure_ignored = (
+        df_azure_not_azure_ignored.to_frame()
+        .rename(columns={0: "azure"})
+        .assign(total=df_total_not_azure_ignored)
+        .assign(
+            percentage=(
+                (df_azure_not_azure_ignored / df_total_not_azure_ignored) * 100
+            ).round(decimals=1)
+        )
+        .dropna()
+    )
+    t_statistic, p_value = stats.ttest_ind(
+        df_both["percentage"],
+        df_both_not_azure_ignored["percentage"],
+        equal_var=False,
+        alternative="less",
+    )
+    alpha = 0.05
+
+    logger.info(
+        "the t_statistic is %i, dependency of group 1 is statistically dependent: %s",
+        t_statistic,
+        p_value < alpha,
     )
